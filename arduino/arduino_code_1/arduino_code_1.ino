ae532d7de81c3d73e952896e3d18aa03de91f9ad
zamsamaz <zamsamazescreve@gmail.com>
2022-01-06 17:39:09 -0200

    tds sensor refactor na metade

--- arduino/arduino_code_1/arduino_code_1.ino
+++ arduino/arduino_code_1/arduino_code_1.ino
@@ -14,13 +14,18 @@
 
 #include <DallasTemperature.h>
 
-#include "GravityTDS.h" 
-
 
 #define TdsSensorPin0 19//a0
 #define TdsSensorPin1 21//a2
 #define TdsSensorPin2 23//A4
 #define TdsSensorPin3 25//A6
+#define VREF 5.0      // analog reference voltage(Volt) of the ADC
+#define SCOUNT  30           // sum of sample point
+int analogBuffer[SCOUNT];    // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0,copyIndex = 0;
+float averageVoltage = 0,temperature = 25;
+float tdsValue0 = 0, tdsValue1 = 0,tdsValue2 = 0,tdsValue3 = 0;
 
 #define MoistSensorPin0 20//A1
 #define MoistSensorPin1 22//A3
@@ -28,11 +33,6 @@
 #define MoistSensorPin3 26//A7
 
 #define ONE_WIRE_BUS 15//D12
-
-GravityTDS gravityTds0;
-GravityTDS gravityTds1;
-GravityTDS gravityTds2;
-GravityTDS gravityTds3;
 
 // Setup a oneWire instance to communicate with any OneWire device
 OneWire oneWire(ONE_WIRE_BUS);
@@ -49,27 +49,10 @@ void setup()
 {
     Serial.begin(115200);
     sensors.begin();	// Start up the library
-
-    gravityTds0.setPin(TdsSensorPin0);
-    gravityTds1.setPin(TdsSensorPin1);
-    gravityTds2.setPin(TdsSensorPin2);
-    gravityTds3.setPin(TdsSensorPin3);
-
-    gravityTds0.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
-    gravityTds1.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
-    gravityTds2.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
-    gravityTds3.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
-
-    gravityTds0.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
-    gravityTds1.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
-    gravityTds2.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
-    gravityTds3.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
-
-
-    gravityTds0.begin();  //initialization
-    gravityTds1.begin();  //initialization
-    gravityTds2.begin();  //initialization
-    gravityTds3.begin();  //initialization
+    pinMode(TdsSensorPin0,INPUT);
+    pinMode(TdsSensorPin1,INPUT);
+    pinMode(TdsSensorPin2,INPUT);
+    pinMode(TdsSensorPin3,INPUT);
 
 }
 
@@ -80,21 +63,27 @@ void loop()
     sensors.requestTemperatures();
     float temperature = sensors.getTempCByIndex(0);
 
+  static unsigned long analogSampleTimepoint = millis();
+   if(millis()-analogSampleTimepoint > 40U)     //every 40 milliseconds,read the analog value from the ADC
+   {
+     analogSampleTimepoint = millis();
+     analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);    //read the analog value and store into the buffer
+     analogBufferIndex++;
+     if(analogBufferIndex == SCOUNT) 
+         analogBufferIndex = 0;
+   }   
+   static unsigned long printTimepoint = millis();
+   if(millis()-printTimepoint > 800U)
+   {
+      printTimepoint = millis();
+      for(copyIndex=0;copyIndex<SCOUNT;copyIndex++)
+        analogBufferTemp[copyIndex]= analogBuffer[copyIndex];
+      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 1024.0; // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+      float compensationCoefficient=1.0+0.02*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+      float compensationVolatge=averageVoltage/compensationCoefficient;  //temperature compensation
+      tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
+ 
 
-    gravityTds0.setTemperature(temperature);  // set the temperature and execute temperature compensation
-    gravityTds1.setTemperature(temperature);  // set the temperature and execute
-    gravityTds2.setTemperature(temperature);  // set the temperature and execute
-    gravityTds3.setTemperature(temperature);  // set the temperature and execute
-
-    gravityTds0.update();  //sample and calculate
-    gravityTds1.update();  //sample and calculate
-    gravityTds2.update();  //sample and calculate
-    gravityTds3.update();  //sample and calculate
-
-    float tdsValue0 = gravityTds0.getTdsValue();  // then get the value
-    float tdsValue1 = gravityTds1.getTdsValue();  // then get the value
-    float tdsValue2 = gravityTds2.getTdsValue();  // then get the value
-    float tdsValue3 = gravityTds3.getTdsValue();  // then get the value
 
     MoistSensorValue0 = analogRead(MoistSensorPin0);
     MoistSensorValue1 = analogRead(MoistSensorPin1);
@@ -105,6 +94,7 @@ void loop()
     if (Serial.available() > 0) {
       String data = Serial.readStringUntil('\n');
       if (data == "sendit"){;
+        
           Serial.print("{\"vaso_1\": { \"tds\": \"");
           Serial.print(tdsValue0);
           Serial.print("\", \"umidade\": \"");
@@ -126,4 +116,30 @@ void loop()
       delay(1000);
     }
 
+}
+
+
+int getMedianNum(int bArray[], int iFilterLen) 
+{
+      int bTab[iFilterLen];
+      for (byte i = 0; i<iFilterLen; i++)
+      bTab[i] = bArray[i];
+      int i, j, bTemp;
+      for (j = 0; j < iFilterLen - 1; j++) 
+      {
+      for (i = 0; i < iFilterLen - j - 1; i++) 
+          {
+        if (bTab[i] > bTab[i + 1]) 
+            {
+        bTemp = bTab[i];
+            bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+         }
+      }
+      }
+      if ((iFilterLen & 1) > 0)
+    bTemp = bTab[(iFilterLen - 1) / 2];
+      else
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+      return bTemp;
 }
